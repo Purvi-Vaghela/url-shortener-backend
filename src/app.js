af011564719f337a445b9318a058567da0759766
Purvi-Vaghela <purvi1800v@gmail.com>
2026-04-03 16:58:23 +0530

    Comment CORS

--- src/app.js
+++ src/app.js
@@ -12,10 +12,10 @@ const app = express();
 // Middleware
 app.use(express.json());
 
- app.use(cors({
-  origin: process.env.FRONTEND_URL
-})); 
-// app.use(cors());
+//  app.use(cors({
+//   origin: process.env.FRONTEND_URL
+// })); 
+app.use(cors());
 
 
 // DB
